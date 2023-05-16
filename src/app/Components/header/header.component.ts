250c94a760f563b74f2ad16405f34805f5873ccf
Yeiko <yeimaraya91@gmail.com>
2023-05-15 19:10:27 -0600

    Correcciones y mejoras

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -18,20 +18,6 @@ export class HeaderComponent implements OnInit {
     
   }
 
-  perfil(){
-    Swal.fire({
-      imageUrl: 'assets/Foto Curriculum 2.png',
-      imageHeight: 100,
-      imageWidth: 100,
-      title: 'Acerca de mi',
-      html: '<p><b>Descripción: </b> Desarrollador web con amplio conocimiento en el Front-end y básico en el Back-end.</p><br>'+
-            '<p><b>Experiencia: </b> Proyecto Final de Graduación (2022) - Agenda de Reservas de Espacios de Trabajo para la Escuela de Ciencias Exactas y Naturales de la UNED</p><br>' +
-            '<p><b>Preparación Académica: </b>Bachiller en Ingeniería Informática - UNED (2022)</p><br>'+
-            '<p><b>Idiomas: </b> Español (Nativo), Inglés (Intermedio)</p>',
-
-      confirmButtonText: 'Aceptar',
-      confirmButtonColor: '#42A5F5'
-    })
-  }
+  
 
 }
